1e00f11533627aea94e02b4ec98cc593bfbb838e
chieetan <mogatai1117@gmail.com>
2025-03-05 20:36:42 +0900

    画像の保存設定を変更

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import torch
 import copy
 import time
@@ -25,10 +27,33 @@ def main():
     pygame.init()
     target_x, target_y = armdef.width / 2, armdef.height / 2 - 150
     model = load_model("data/controlnet_xy_and_theta.pth")
+    #円状のパスを生成（各要素は[x,y]の座標）
+    # target_path = draw_circle()
+    # print(f"target path length: {len(target_path)}")
+    # for pos in target_path:
+    #     target_x, target_y = pos
     generate_control_signals(target_x, target_y, model)
+    pygame.time.wait(50)
     pygame.quit()
+    
 
 
+def draw_circle():
+    #円の軌道を描くための座標を格納するリストpathを設定
+    path= []
+    #円の中心のy座標をarmdef.width/2-100に設定
+    y0 = armdef.height/2-100
+    #円の中心のx座標をarmdef.height/2に設定
+    x0 = armdef.width/2
+    #円の半径を150に設定
+    r = 150
+    # 円の軌道を描くための座標を格納するリストlに座標を追加
+    circle = np.arange(0,360,0.1)
+    for i in tqdm.tqdm(range(len(circle))):
+        x = x0 + r * np.cos(np.radians(i))
+        y = y0 + r * np.sin(np.radians(i))
+        path.append([x,y])
+    return path
 # ----------------------------------------------------------
 # Load a model from a given path and put it on GPU
 # ----------------------------------------------------------
@@ -37,6 +62,10 @@ def load_model(model_path):
     model.load_state_dict(torch.load(model_path))
     return model.cuda()
 
+#このファイルと同一のパス画像を保存する関数を定義
+def save_image(display, filename):
+    pygame.image.save(display, filename)
+    
 
 # ----------------------------------------------------------
 # Generate control signals, visualize and plot data.
@@ -48,9 +77,9 @@ def generate_control_signals(target_x, target_y, model):
     display = pygame.display.set_mode((armdef.width, armdef.height))
 
     # Build the list of target theta values.
-    target_thetas = [(3.14 / 2) * (i / 50) for i in tqdm.tqdm(range(-50, 50))]
+    target_thetas = [(3.14 / 2) * (i / 10) for i in tqdm.tqdm(range(-10, 10))]
     target_thetas = target_thetas + target_thetas[::-1]
-    target_thetas *= 4
+    target_thetas *= 5
     print(f"target thetas length: {len(target_thetas)}")
 
     # Compute control signals for all target thetas.
@@ -66,16 +95,29 @@ def generate_control_signals(target_x, target_y, model):
     df = pd.DataFrame(xt_all_runs_reshaped)
 
     # Apply a moving average filter.
-    df_filtered = moving_average_filter(df)
+    df_filtered = df
 
     # Plot both original and filtered data.
     # plot_data(df, df_filtered)
+    
+   # 既存の出力ファイルを取得
+    existing_files = {f"output_frame_{i}.png" for i in range(15, 600, 40)}
 
-    # Iterate over the filtered dataframe to draw arm images.
     for i in range(df_filtered.shape[0]):
-        xt = df_filtered.iloc[i].to_list()
-        print(f"xt: {xt}")
-        draw_arm(target_x, target_y, xt, target_thetas[i], display)
+        if i in range(15, 600, 120):
+            display.fill((255, 255, 255))  # 背景を白にリセット
+            xt = df_filtered.iloc[i].to_list()
+            print(f"xt: {xt}")
+
+            draw_arm(target_x, target_y, xt, target_thetas[i], display, (0, 0, 255, int((i / 600) * 255)))
+            save_image(display, f"output_frame_{i}.png")  # 画像を保存
+
+    # # 不要なPNGファイルを削除
+    # for file in os.listdir():
+    #     if file.startswith("output_frame_") and file.endswith(".png"):
+    #         if file not in existing_files:
+    #             os.remove(file)
+    #             print(f"Deleted: {file}")
 
 
 # ----------------------------------------------------------
@@ -134,9 +176,8 @@ def plot_data(df, df_filtered):
 # ----------------------------------------------------------
 # Draw the arm on the display based on the control signal xt.
 # ----------------------------------------------------------
-def draw_arm(x, y, xt, theta, display):
+def draw_arm(x, y, xt, theta, display,color):
     armdef.arm.calc(xt)
-    display.fill((255, 255, 255))
     pygame.draw.line(
         display,
         (255, 0, 0),
@@ -144,20 +185,20 @@ def draw_arm(x, y, xt, theta, display):
         (np.cos(np.pi / 2 - theta) * 70 + x, np.sin(np.pi / 2 - theta) * 70 + y),
         5,
     )
-    armdef.arm.draw(display)
-    font = pygame.font.Font(None, 24)
-    text1 = font.render(
-        f"result: {armdef.arm.last.x[1] / np.pi * 180} degree", True, (0, 0, 0)
-    )
-    text2 = font.render(f"target: {theta / np.pi * 180} degree", True, (0, 0, 0))
-    text3 = font.render(
-        f"error: {abs(armdef.arm.last.x[1] - theta) / np.pi * 180} degree",
-        True,
-        (0, 0, 0),
-    )
-    display.blit(text1, (10, 10))
-    display.blit(text2, (10, 40))
-    display.blit(text3, (10, 70))
+    armdef.arm.draw(display,color)
+    # font = pygame.font.Font(None, 24)
+    # text1 = font.render(
+    #     f"result: {armdef.arm.last.x[1] / np.pi * 180} degree", True, (0, 0, 0)
+    # )
+    # text2 = font.render(f"target: {theta / np.pi * 180} degree", True, (0, 0, 0))
+    # text3 = font.render(
+    #     f"error: {abs(armdef.arm.last.x[1] - theta) / np.pi * 180} degree",
+    #     True,
+    #     (0, 0, 0),
+    # )
+    # display.blit(text1, (10, 10))
+    # display.blit(text2, (10, 40))
+    # display.blit(text3, (10, 70))
     pygame.draw.circle(display, (0, 0, 0), (int(x), int(y)), 10)
     pygame.display.update()
     pygame.time.wait(50)
@@ -186,4 +227,12 @@ def spline_interpolation(df, n_points=100):
 
 
 if __name__ == "__main__":
+    # display = pygame.display.set_mode((armdef.width, armdef.height))
+    # display.fill((255, 255, 255))
+    # color = (0,0,255,100)
+    # pygame.draw.line(display, color, (0,0), (500,200), 5)
+    # color = (0,0,255,200)
+    # pygame.draw.line(display, color, (0,0), (200,500), 5)
+    # pygame.display.update()
+    # time.sleep(4)
     main()
