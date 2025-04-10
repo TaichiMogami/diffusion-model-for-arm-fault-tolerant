142735e42570df7dd07eca0f2f9fead50430dbc6
TaichiMogami <mogami.taichi550@mail.kyutech.jp>
2025-04-10 12:52:33 +0900

    improve accuracy

--- gen_data.py
+++ gen_data.py
@@ -1,19 +1,21 @@
-from train_data_related import gen_data
+import os
+import random
+
 import pandas as pd
 from tqdm import tqdm
-import random
-import os
+
+from train_data_related import gen_data
 
 # 教師データを生成してdata/train.csvに保存する
-if __name__ == '__main__':
+if __name__ == "__main__":
     data_list = []
-    for i in tqdm(range(100000)):
+    for i in tqdm(range(100000000)):
         data_list.append(gen_data(random.randint(300, 980), random.randint(100, 600)))
 
     df = pd.DataFrame(data_list)
 
-    #dataフォルダが存在しない場合、dataフォルダを作成
-    if not os.path.exists('data'):
-        os.mkdir('data')
-    #data/train.csvにデータを保存
-    df.to_csv('data/train.csv', index=False)
+    # dataフォルダが存在しない場合、dataフォルダを作成
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    # data/train.csvにデータを保存
+    df.to_csv("data/train.csv", index=False)
